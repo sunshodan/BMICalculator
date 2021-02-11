a8fde9816de3c135d448dfd42c53a19737082285
sunshodan <61916077+sunshodan@users.noreply.github.com>
2021-02-11 22:08:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./components/style.css";
+// import { useState } from "react";
+import Material from "./components/Material.js";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>BMI Calculator</h1>
+      <Material />
+    </div>
+  );
+}
---
+++ src/components/Material.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Material = () => {
+  const [height, setHeight] = useState(10);
+  const [weight, setWeight] = useState(0);
+  const [ans, setAns] = useState("");
+  // useEffect(() => {
+  //   setAns(weight / (height * height));
+  // }, [weight, height]);
+  const weightHandler = (e) => {
+    setWeight(e.target.value);
+  };
+  const heightHandler = (e) => {
+    setHeight(e.target.value);
+  };
+  const buttonHandler = () => {
+    setAns(`BMI is ${Math.round(weight / (height * height))}`);
+  };
+  return (
+    <>
+      <form className="f1">
+        <label className="l1">weight</label>
+        <input className="textbox" onChange={weightHandler} type="text" />
+      </form>
+      <form className="f1">
+        <label className="l1">height</label>
+        <input className="textbox" onChange={heightHandler} type="text" />
+      </form>
+      <button className="f1 btn" onClick={buttonHandler} type="submit">
+        Submit
+      </button>
+      <h2>{ans}</h2>
+    </>
+  );
+};
+export default Material;
